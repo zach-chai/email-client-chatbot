d9f39566fcc8c73ef688e5efc232f250850e4487
Zachary Chai <zachary.chai@outlook.com>
2016-09-04 13:41:22 -0400

    basic parsing

--- app/assets/javascripts/chatbot.js.coffee
+++ app/assets/javascripts/chatbot.js.coffee
@@ -1,8 +1,14 @@
+send = ->
+   msg = $("#chatbot-message").val()
+   $.get "/chatbot?message=#{msg}", (data) ->
+      $("#message-list").append "<li>#{data.message}</li>"
+
 setupchatbot = ->
    $("#chatbot-submit").click ->
-      msg = $("#chatbot-message").val()
-      $.get "/chatbot?message=#{msg}", (data) ->
-         $("#message-list").append "<li>#{data.message}</li>"
+      send()
+   $("#chatbot-message").keypress (ev) ->
+    if ev.which == 13
+      send()
 
 $(document).on 'turbolinks:load', ->
    setupchatbot()
--- app/controllers/chatbot_controller.rb
+++ app/controllers/chatbot_controller.rb
@@ -5,12 +5,37 @@ class ChatbotController < ApplicationController
   # POST /chatbots
   def message
     msg = params[:message] || 'Hello user'
-    msg = msg.gsub(/[^0-9a-z ]/i, '')
+    # msg = msg.gsub(/[^0-9a-z ]/i, '')
+    msg = parse_rules msg
 
     @res = msg
   end
 
   def test
 
+  end
+
+  private
+
+  FETCH_KEYS = ['fetch', 'get', 'find', 'open', 'show', 'view']
+
+  def parse_rules(msg)
+    msg = Textoken(msg.downcase).tokens
+    if msg.include? 'help'
+      'help'
+    elsif include_any? msg, FETCH_KEYS
+      'fetch email'
+    else
+      msg
+    end
+  end
+
+  def include_any?(msg, keys)
+    keys.each do |key|
+      if msg.include? key
+        return true
+      end
+    end
+    false
   end
 end
