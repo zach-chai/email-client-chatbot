380fdda206090ce8855d71b2dd09642eba7597bb
Zachary Chai <zachary.chai@outlook.com>
2016-09-06 00:22:28 -0400

    testing parsing heuristics

--- app/controllers/chatbot_controller.rb
+++ app/controllers/chatbot_controller.rb
@@ -5,10 +5,12 @@ class ChatbotController < ApplicationController
   # POST /chatbots
   def message
     msg = params[:message] || 'Hello user'
-    # msg = msg.gsub(/[^0-9a-z ]/i, '')
-    msg = parse_rules msg
+    msg = Textoken(msg.downcase).tokens
 
-    @res = msg
+    @res = parse_rules msg
+    @res = "#{@res}: #{ordered_tokens_heuristic(msg, [FETCH_TOKENS, EMAIL_TYPES])}"
+
+    @res
   end
 
   def test
@@ -17,15 +19,16 @@ class ChatbotController < ApplicationController
 
   private
 
-  FETCH_KEYS = ['fetch', 'get', 'find', 'open', 'show', 'view']
+  FETCH_TOKENS = ['fetch', 'get', 'find', 'open', 'show', 'view', 'what', 'tell', 'give', 'read']
   EMAIL_TYPES = ['unread', 'recent', 'today', 'yesterday']
   #TODO detect if email address and fetch those emails
 
   def parse_rules(msg)
-    msg = Textoken(msg.downcase).tokens
     if msg.include? 'help'
       'help'
-    elsif include_any? msg, FETCH_KEYS
+    elsif ordered_tokens_exist? msg, [FETCH_TOKENS, EMAIL_TYPES]
+      "fetch emails ordered_tokens"
+    elsif include_any? msg, FETCH_TOKENS
       if email_type = include_any?(msg, EMAIL_TYPES)
         "fetch emails #{email_type}"
       else
@@ -36,12 +39,35 @@ class ChatbotController < ApplicationController
     end
   end
 
-  def include_any?(msg, keys)
-    keys.each do |key|
-      if msg.include? key
-        return key
+  def include_any?(msg, tokens)
+    tokens.each do |token|
+      if msg.include? token
+        return token
       end
     end
     false
+  end
+
+  def ordered_tokens_exist?(msg, tokens, strict = false)
+    msg.each do |msg_token|
+      if tokens.first.include? msg_token
+        tokens.shift
+      end
+    end
+    if tokens.empty?
+      true
+    else
+      false
+    end
+  end
+
+  def ordered_tokens_heuristic(msg, tokens, opts = {})
+    tokens_length = tokens.length
+    msg.each do |msg_token|
+      if tokens.first.include? msg_token
+        tokens.shift
+      end
+    end
+    (tokens_length - tokens.length).to_f / tokens_length.to_f
   end
 end
