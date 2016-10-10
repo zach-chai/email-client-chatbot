77562b7cde853eff0a3c7e77a0211e347c91d5d3
Zachary Chai <zachary.chai@outlook.com>
2016-10-10 16:43:21 -0400

    create chatbot email handler

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   resources :emails
   get "/chatbot", to: "chatbot#message", defaults: {format: 'json'}
+  get "/chatbot/emails", to: "chatbot#emails", defaults: {format: 'json'}
   get "/testbot", to: "chatbot#test"
   get "/demo", to: "emails#demo"
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
